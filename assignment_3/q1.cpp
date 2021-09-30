a96eee63ca1a89e240f3edd9128e624db17d588f
Samuel <gsk0007@auburn.edu>
2021-09-30 14:46:30 -0500

    q1 progress

--- assignment_3/q1.cpp
+++ assignment_3/q1.cpp
@@ -1,22 +1,46 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+const int charLen = 20;
+//todo Hey man, this shit is fucked. Please convert code to use a char array instead of a string
 int main() {
-	string first, second;
-
-	int added[20];
-    
+	//string first, second;
+	char first[charLen];
+	char second[charLen];
+	int added[21];
 	int carry = 0;
+	int counter;
+
 	// Get the values
 	cout << "Enter the first number:";
 	cin  >> first;
+	cout << first;
 	cout << "Enter the second number:";
-	cin  >> second;	
+	cin  >> second;
 
 	// Convert digits to ints
 	int x[first.length()], y[second.length()];
-
 	
+	// Define a counter 
+	if(first.length() <= second.length()){
+		counter = second.length();
+	}else{
+		counter = first.length();
+	};
+
+	for(int i = first.length(); i >= 0; i--){
+		x[i] = first[i];
+		cout << x[i];
+	};
+	cout << endl;
+
+	for(int i = second.length(); i >= 0; i--){
+		y[i] = second[i];
+		cout << x[i];
+	};
+	cout << endl;
+
 	// Add the arrays
 
 	return 0;
