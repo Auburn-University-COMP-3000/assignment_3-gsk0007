b276f2d02f194e791be6b410f1d09c8694441424
Samuel <gsk0007@auburn.edu>
2021-10-02 12:11:21 -0500

    q3 work

--- assignment_3/q3.cpp
+++ assignment_3/q3.cpp
@@ -8,12 +8,20 @@ void sortArray(int arr[], int size);
 
 int main() {
 	int size = 0;
-	    
 	int arr[50];
+	int temp;
 
 	// Read in the values into an array
-	cout << "Enter values ended with : ";
+	cout << "Enter values ended with -1: ";
+	cin >> temp;
 	
+	while(size<50 && temp!=-1){
+		arr[size] = temp;
+		size++;
+		cin >> temp;
+	};
+
+	// display sorted array nums with how many times they appear
 
 	sortArray(arr,size);
 
@@ -32,6 +40,15 @@ int countOccurances(int *arr, int size, int n) {
 void sortArray(int *arr, int size) {
 	int temp = 0;
 
+	for(int i=0; i<size-1;i++){
+		for(int j=1; j<size;j++){
+			if(arr[j]<arr[i]){
+				temp = arr[j];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
 
 }
 
